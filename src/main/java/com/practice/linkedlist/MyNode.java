ec05440b4533a27a5816efa4df22f4dd9aea5ad1
Kushagra5678 <kushu.7feb@gmail.com>
2020-10-07 21:40:58 +0530

    modify: able to add nodes to LL

---
+++ src/main/java/com/practice/linkedlist/MyNode.java
@@ -0,0 +1,19 @@
+package com.practice.linkedlist;
+
+public class MyNode<K> {
+	private K key;
+	private MyNode<K> next;
+	
+	public MyNode(K key) {
+		this.key= key;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+	
+}
